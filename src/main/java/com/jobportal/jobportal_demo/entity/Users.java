ca9e7caa0472378588b5e5dba4d9c6b6b97cb355
Qingyang Zhang <zqy1jason@gmail.com>
2025-01-08 16:15:51 -0500

    UI File

---
+++ src/main/java/com/jobportal/jobportal_demo/entity/Users.java
@@ -0,0 +1,36 @@
+package com.jobportal.jobportal_demo.entity;
+
+import java.util.Date;
+
+import org.hibernate.usertype.UserType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+    private String email;
+    private String password;
+    private boolean isActive;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+}   
