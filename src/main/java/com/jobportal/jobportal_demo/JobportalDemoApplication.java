a06b91040df29ff3081b07f248b45592ec098dd8
Qingyang Zhang <zqy1jason@gmail.com>
2025-01-07 23:11:54 -0500

    first commit

---
+++ src/main/java/com/jobportal/jobportal_demo/JobportalDemoApplication.java
@@ -0,0 +1,19 @@
+package com.jobportal.jobportal_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JobportalDemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JobportalDemoApplication.class);
+	}
+}
---
+++ src/test/java/com/jobportal/jobportal_demo/JobportalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobportal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
